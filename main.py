d4ab6beabd9d0e780c5c89ee1d78d45ee6189336
Samrat D <samratapr40@gmail.com>
2024-02-18 14:47:32 +0530

    decorating the main.py screen

--- main.py
+++ main.py
@@ -21,12 +21,25 @@ def main():
         page_icon="📚"
     )
 
+    # removing header space
+    st.write('<style>div.block-container{padding-top:2rem;}</style>', unsafe_allow_html=True)
+
+    # to hide hamburger icon and footer 
+    hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+    st.markdown(hide_st_style, unsafe_allow_html=True)
+
     # webpage title and description 
     st.markdown("<h1 style='text-align: center;'>Chat With Docs</h1>", unsafe_allow_html=True)
     st.markdown("<p style='text-align: center;'>You can collaborate with your data</p>", unsafe_allow_html=True)    
     
     # file upload 
-    upload_file = st.file_uploader("Upload documents")
+    upload_file = st.file_uploader("Upload multiple documents",accept_multiple_files=True)
     
     # chatting container bot and user
     chat_container = st.container()
@@ -38,7 +51,10 @@ def main():
         
     # buttons and text inputs
     user_input = st.text_input("Ask your queries", placeholder="enter your queries", on_change=set_send_input, key="user_input")
-    sendButton = st.button("send", key="sendButton")
+    
+    col1, col2 = st.columns(2)
+    sendButton = col1.button("send", key="sendButton")
+    resetButton = col2.button("reset", key="resetButton")
 
     # onclick event for button and put enter on text input
     if sendButton or set_send_input:
