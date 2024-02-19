73064d59e75cd7815e3dc06ac95838d0480ae1b9
Samrat D <samratapr40@gmail.com>
2024-02-19 08:35:52 +0530

    application was build --completed --tested

--- main.py
+++ main.py
@@ -1,20 +1,70 @@
-# impoting the required packages 
+# # impoting the required packages
 import streamlit as st
+import os
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain_community.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI 
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
 
+# gemini api 
+load_dotenv()
+genai.configure(api_key = os.getenv("GOOGLE_API_KEY"))         
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 10000, chunk_overlap = 1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks,embeddings) 
+    vector_store.save_local("faiss_index") 
+
+def get_conversation_chain():
+    # define the prompt
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+    model = ChatGoogleGenerativeAI(model = "gemini-pro", temperatue = 0.3)
+    prompt = PromptTemplate(template = prompt_template, input_variables= ["context","question"])
+    chain = load_qa_chain(model,chain_type="stuff",prompt = prompt)
+    return chain
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    new_db = FAISS.load_local("faiss_index", embeddings)
+    docs = new_db.similarity_search(user_question)
+    chain = get_conversation_chain()
+    response = chain(
+        {"input_documents":docs, "question": user_question}
+        , return_only_outputs=True)
+    print(response)
+    st.write("Reply: ", response["output_text"])
 
 # to clear typed text 
 def clear_input_field():
-    st.session_state.user_question = st.session_state.user_input
-    st.session_state.user_input = ""
-
-# sending the input 
-def set_send_input():
-    st.session_state.send_input = True
-    clear_input_field()
+    st.session_state.user_input1 = ""
 
 # main function 
-def main():
-    
+def main():    
     # for website name and icon 
     st.set_page_config(
         page_title="Chat With Docs",
@@ -37,32 +87,22 @@ def main():
     # webpage title and description 
     st.markdown("<h1 style='text-align: center;'>Chat With Docs</h1>", unsafe_allow_html=True)
     st.markdown("<p style='text-align: center;'>You can collaborate with your data</p>", unsafe_allow_html=True)    
-    
-    # file upload 
-    upload_file = st.file_uploader("Upload multiple documents",accept_multiple_files=True)
-    
-    # chatting container bot and user
-    chat_container = st.container()
 
-    # states of the text input to clear
-    if 'send_input' not in st.session_state:
-        st.session_state.send_input = False
-        st.session_state.user_question = ""
-        
-    # buttons and text inputs
-    user_input = st.text_input("Ask your queries", placeholder="enter your queries", on_change=set_send_input, key="user_input")
+    with st.sidebar:    
+        pdf_docs = st.file_uploader("Upload your PDF Files", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+
+    user_question = st.text_input("Ask a Question:", placeholder="enter your queries")
+
+
+    if user_question:
+        user_input(user_question)
+
     
-    col1, col2 = st.columns(2)
-    sendButton = col1.button("send", key="sendButton")
-    resetButton = col2.button("reset", key="resetButton")
-
-    # onclick event for button and put enter on text input
-    if sendButton or set_send_input:
-        if st.session_state.user_question != "":
-            llm_response = "this is the response from the llm model"
-            with chat_container:
-                st.chat_message("user").write(st.session_state.user_question)
-                st.chat_message("bot").write(llm_response)
-
 if __name__ == '__main__':
     main()
